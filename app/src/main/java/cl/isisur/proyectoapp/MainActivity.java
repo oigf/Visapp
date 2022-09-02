aef15ad9cd4f226434873b52215ff9d7464de729
oigf <garrido.osvaldo1@gmail.com>
2022-09-02 17:19:17 -0400

    1.2

--- app/src/main/java/cl/isisur/proyectoapp/MainActivity.java
+++ app/src/main/java/cl/isisur/proyectoapp/MainActivity.java
@@ -2,13 +2,28 @@ package cl.isisur.proyectoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button ventana;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ventana=(Button)findViewById(R.id.ventana);
+
+        ventana.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent( MainActivity.this, activity2.class);
+                startActivity(i);
+            }
+        });
     }
 }
